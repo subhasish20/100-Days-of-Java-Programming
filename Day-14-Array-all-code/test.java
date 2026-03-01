73331613139115f7bb3a5bb5c03d23777f28576f
subhasish20 <subhasishjena8280@gmail.com>
2026-03-01 23:05:10 +0530

    CJP

---
+++ Day-14-Array-all-code/test.java
@@ -0,0 +1,19 @@
+public class test
+{
+    /*
+    Array declaration, creation and initialization
+    */
+    public static void main(String[] args) {
+
+        int[] x; // array declaratin
+
+        x = new int[3]; // array creation
+
+        // array initialization
+        x[0] = 5;
+        x[1] = 6;
+        x[2] = 7;
+
+        System.out.println(x[0]); // 5
+    }
+}
---
+++ Day-14-Array-all-code/test2.java
@@ -0,0 +1,10 @@
+public  class test2
+{
+    public static void main(String[] args) {
+        /* array declaration, creation and initialization in one line*/
+
+        int[] x = {10,20,30};
+
+        System.out.println(x[0]); // 10
+    }
+}
